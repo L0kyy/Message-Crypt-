8117cbc4077ea67edae2384003661f90b8fc865e
L0kyy <57066338+L0kyy@users.noreply.github.com>
2019-10-27 15:24:16 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,7 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+bot.on('ready', function () {
+    console.log("Je suis connecté !")
+    })
+    
+    bot.login('TONjMwMTcwMjk0OTEyMjg2NzQx.XbWhYg.8emegMv2Ivq-ukkTay_tDsowFlk')
